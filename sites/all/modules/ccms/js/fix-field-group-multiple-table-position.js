7f4cc439f44bbf91af4c981a8dc2c19e51fe5e4d
daveshaw <dave@springboardinternet.co.uk>
2013-06-11 14:19:53 +0100

    Comment added to Javascript

--- sites/all/modules/ccms/js/fix-field-group-multiple-table-position.js
+++ sites/all/modules/ccms/js/fix-field-group-multiple-table-position.js
@@ -1,4 +1,6 @@
 jQuery(document).ready(function ($) {
+
+  // correct field mis-positioning. See https://drupal.org/node/1650132.
   
   $('#fgm_node_module_version_form_group_assessment_methods-add-more-wrapper').insertAfter('#edit-field-mod-assess-strategy');
   $('#fgm_node_module_version_form_group_clo-add-more-wrapper').insertAfter('#form-clo-title');
